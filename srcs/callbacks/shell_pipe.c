16cc4eee0c143a35fb57ee827f56f33ddd84d0e9
Mohammed MERABET <dgny50@gmail.com>
2018-09-17 17:18:21 +0200

    some corrections

--- srcs/callbacks/shell_pipe.c
+++ srcs/callbacks/shell_pipe.c
@@ -14,8 +14,20 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include "ft_printf.h"
+#include "ft_str.h"
 
-static pid_t	new_fork(t_ast *ast, void *res, t_iterf *iterf, int fd[2])
+static int		has_left_redir(t_ast *ast)
+{
+	if (!ast)
+		return (0);
+	if (ast->type == TK_REDIR && ft_strmatch(ast->name, "*<*"))
+		return (1);
+	else if (has_left_redir(ast->left) || has_left_redir(ast->right))
+		return (1);
+	return (0);
+}
+
+static pid_t	new_fork(t_ast *ast, void *res, t_iterf *iterf, int fd[3])
 {
 	pid_t	pid;
 	pid_t	save;
@@ -24,11 +36,14 @@ static pid_t	new_fork(t_ast *ast, void *res, t_iterf *iterf, int fd[2])
 	if (!ast || !ast->name)
 		return (-1);
 	ir = (ast == ast->parent->right ? 1 : 0);
+	if (ir && has_left_redir(ast))
+		fd[2] = 1;
 	if (!(pid = fork()))
 	{
 		save = getpgrp();
 		close(fd[ir]);
-		dup2(fd[!ir], !ir);
+		if (!fd[2])
+			dup2(fd[!ir], !ir);
 		handle_pgid();
 		ft_astiter(ast, res, iterf);
 		tcsetpgrp(0, save);
@@ -40,25 +55,26 @@ static pid_t	new_fork(t_ast *ast, void *res, t_iterf *iterf, int fd[2])
 
 int				shell_pipe_cb(t_ast *ast, void **op, void *res, t_iterf *iterf)
 {
-	int		fd[2];
+	int		fd[3];
 	pid_t	pidl;
 	pid_t	pidr;
 	int		leftres;
 	int		rightres;
 
 	(void)op;
-	leftres = 0;
+	fd[2] = 0;
 	if (pipe(fd) == -1)
 		return (SH_PIPFAIL);
 	if ((pidr = new_fork(ast->right, &rightres, iterf, fd)) == -1
 			&& close(fd[1]) != 1 && close(fd[0]) != 1)
 		return (SH_FORKFAIL);
-	if ((pidl = new_fork(ast->left, &leftres, iterf, fd)) == -1
+	if (!fd[2] && (pidl = new_fork(ast->left, &leftres, iterf, fd)) == -1
 			&& close(fd[1]) != 1 && close(fd[0]) != 1)
 		return (SH_FORKFAIL);
 	close(fd[1]);
 	close(fd[0]);
-	waitpid(pidl, NULL, 0);
+	if (!fd[2])
+		waitpid(pidl, NULL, 0);
 	waitpid(pidr, res, 0);
 	*(int *)res = WEXITSTATUS(*(int *)res);
 	return (0);
