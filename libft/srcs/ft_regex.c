613210b3e747cf6044f0cf491a011b1027517ff0
Mohammed MERABET <dgny50@gmail.com>
2018-09-25 02:07:23 +0200

    regex improvements

--- libft/srcs/ft_regex.c
+++ libft/srcs/ft_regex.c
@@ -1,23 +1,46 @@
 #include "ft_str.h"
 #include "ft_printf.h"
+/*
+static int	default_match(const char **regex, const char **str, const char *arg)
+{
+	
+}
+*/
+static int	expanded_wildcard(const char **regex, const char **str, int *len)
+{
+	int		jmp;
+	char	*fct;
+
+	(void)str;
+	(void)len;
+	if ((jmp = ft_strbetweenps_ext((char **)regex, "[?];{?}")) != -1)
+	{
+		fct = ft_strnchrl(*regex, '@', jmp);
+		if (ft_strchr("=><]", fct[1]))
+		{
+			
+		}
+
+
+		// ft_printf("at: '%d' '%s'\n", jmp, ft_strnchrl(*regex, '@', jmp));
+		// *[argument@function_name{=><}{0-9}]
+		// ?[a-z] == *[a-z@=] == *[a-z@=1]
+		*regex += jmp;
+		return (0);
+	}
+	return (-1);
+}
 
 static int	wildcard(const char **regex, const char **str, int *len)
 {
-	int		jmp;
-
 	while ((*regex)[0] == '*' && (*regex)[1] == '*')
 		++*regex;
 	if (**regex == '*')
 	{
 		if (*++*regex == '[')
 		{
-			if ((jmp = ft_strbetweenps_ext((char **)regex, "[?];{?}")) != -1)
-			{
-				//ft_printf("at: '%d' '%s'\n", jmp, ft_strnchrl(*regex, '@', jmp));
-				// *[argument@function_name{=><}{0-9}]
-				*regex += jmp;
+			if (expanded_wildcard(regex, str, len) != -1)
 				return (0);
-			}
 		}
 		while (**str && ft_regex(*regex, *str) == -1)
 		{
