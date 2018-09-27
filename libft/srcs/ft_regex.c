13972ec63a7f561e75d69e8a0590ef35f96bf623
Mohammed MERABET <dgny50@gmail.com>
2018-09-27 14:44:53 +0200

    regex improvements

--- libft/srcs/ft_regex.c
+++ libft/srcs/ft_regex.c
@@ -1,27 +1,28 @@
 #include "ft_str.h"
+#include "ft_mem.h"
 #include "ft_printf.h"
-
+/*
 static int	default_match(const char *regex, const char **str, const char *arg)
 {
 	
 }
-
-static int	expanded_wildcard(const char **regex, const char **str, int *len)
+*/
+static int	expanded_wildcard(t_regex_info *regex_info, int *len)
 {
 	int		jmp;
 	char	*fct;
 	int		ret;
 
-	(void)str;
 	(void)len;
-	if ((jmp = ft_strbetweenps_ext((char **)regex, "[?];{?}")) != -1)
+	if ((jmp = ft_strbetweenps_ext((char **)&regex_info->regex, "[?];(*)")) != -1)
 	{
-		fct = ft_strnchrl(*regex, '@', jmp);
+		fct = ft_strnchrl(regex_info->regex, '@', jmp);
 		if (ft_strchr("=><]", fct[1]))
 		{
-			ret = default_match(regex, str, len);
+			ret = 0;//default_match(regex, str, len);
 		}
-		/*
+	
+	/*
 		"upper:?[A-Z]"
 		"lower:?[a-z]"
 		"digit:?[0-9]"
@@ -39,61 +40,86 @@ static int	expanded_wildcard(const char **regex, const char **str, int *len)
 		// ft_printf("at: '%d' '%s'\n", jmp, ft_strnchrl(*regex, '@', jmp));
 		// *[argument@function_name{=><}{0-9}]
 		// ?[a-z] == *[a-z@=] == *[a-z@=1]
-		*regex += jmp;
-		return (0);
+		regex_info->regex += jmp;
+		return (ret);
 	}
 	return (-1);
 }
 
-static int	wildcard(const char **regex, const char **str, int *len)
+static int	wildcard(t_regex_info *regex_info, int *len)
 {
-	while ((*regex)[0] == '*' && (*regex)[1] == '*')
-		++*regex;
-	if (**regex == '*')
+	int			ret;
+	const char	*o_str;
+	const char	*o_rgx;
+
+	while (regex_info->regex[0] == '*' && regex_info->regex[1] == '*')
+		++regex_info->regex;
+	if (*regex_info->regex == '*')
 	{
-		if (*++*regex == '[')
+		if (*++regex_info->regex == '[' && expanded_wildcard(regex_info, len) != -1)
+			return (0);
+		ret = 0;
+		o_str = regex_info->str;
+		o_rgx = regex_info->regex;
+		while (*o_str && (ret = regex_exec(regex_info)) == -1)
 		{
-			if (expanded_wildcard(regex, str, len) != -1)
-				return (0);
-		}
-		while (**str && ft_regex(*regex, *str) == -1)
-		{
-			++*str;
+			++o_str;
 			++*len;
+			regex_info->str = o_str;
+			regex_info->regex = o_rgx;
 		}
+		regex_info->str = o_str;
+		regex_info->regex = o_rgx;
+		if (ret != -1)
+			return (ret);
 	}
 	else
 	{
-		if (!**str)
+		if (!*regex_info->str)
 			return (-1);
-		++*str;
+		++regex_info->str;
 		++*len;
-		++*regex;
+		++regex_info->regex;
 	}
 	return (0);
 }
-
-int	ft_regex(const char *regex, const char *str)
+int		regex_exec(t_regex_info *regex_info)
 {
 	int	pos;
 	int	len;
 
 	len = 0;
-	while (*regex)
+	while (*regex_info->regex && *regex_info->str)
 	{
-		if (ft_strchr("*?", *regex))
+		if (*regex_info->regex == '*' || *regex_info->regex == '?')
 		{
-			if (wildcard(&regex, &str, &len) == -1)
+			if ((pos = wildcard(regex_info, &len)) == -1)
 				return (-1);
+			else if (pos > 0)
+				return (len + pos);
 			continue ;
 		}
-		if ((pos = ft_strpbrkl_pos(regex, "*?")) == -1)
-			pos = ft_strlen(regex);
-		if (!ft_strnequ(regex, str, pos))
+		if ((pos = ft_strpbrkl_pos(regex_info->regex, "*?")) == -1)
+			pos = ft_strlen(regex_info->regex);
+		if (!ft_strnequ(regex_info->regex, regex_info->str, pos))
 			return (-1);
 		len += pos;
-		str += pos;
-		regex += pos;
+		regex_info->str += pos;
+		regex_info->regex += pos;
 	}
-	return (*str ? -1 : len);
+	if (*regex_info->regex && !ft_strequ(regex_info->regex, "*"))
+		return (-1);
+	return (*regex_info->str ? -1 : len);
+}
+
+int	ft_regex(const char *regex, const char *str)
+{
+	t_regex_info	regex_info;
+
+	ft_bzero(&regex_info, sizeof(regex_info));
+	regex_info.str_begin = str;
+	regex_info.rgx_begin = regex;
+	regex_info.str = str;
+	regex_info.regex = regex;
+	return (regex_exec(&regex_info));
 }
