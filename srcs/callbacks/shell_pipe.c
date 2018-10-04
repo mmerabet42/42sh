b0b69e89479e31b6f0ed290ecac7b92a55b70470
Jeremi RAYMOND <jraymond@e3r2p4.42.fr>
2018-10-04 21:19:31 +0200

    ctrl+z bug all rest clean

--- srcs/callbacks/shell_pipe.c
+++ srcs/callbacks/shell_pipe.c
@@ -6,7 +6,7 @@
 /*   By: jraymond <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 19:45:28 by jraymond          #+#    #+#             */
-/*   Updated: 2018/10/04 16:16:59 by jraymond         ###   ########.fr       */
+/*   Updated: 2018/10/04 21:17:58 by jraymond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,96 +20,6 @@
 
 #include "parser.h"
 
-/*static int		check_cond(t_ast *ast, int type)
-{
-	if (type)
-	{
-		if (!ast->parent || !ast->parent->name
-				|| !ft_strequ(ast->parent->name, "|"))
-			return (1);
-		else if (ft_strequ(ast->left->name, "|"))
-			return (1);
-		return (0);
-	}
-	if (!ast)
-		return (0);
-	if (ast->type == TK_REDIR && ft_strmatch(ast->name, "*<*"))
-		return (1);
-	else if (check_cond(ast->left, 0) || check_cond(ast->right, 0))
-		return (1);
-	return (0);
-}
-
-static pid_t	son_fork(pid_t pgrp, int fd[3], t_ast *ast, t_iterf *iterf)
-{
-	int	ir;
-	int	pid;
-	int	res;
-
-	ir = (ast == ast->parent->right ? 1 : 0);
-	if (!(pid = fork()))
-	{
-		setpgid(0, (!pgrp ? 0 : pgrp));
-		close(fd[ir]);
-		if (!fd[2])
-			dup2(fd[!ir], !ir);
-		g_shell->bits |= (1 << 1);
-		ft_astiter(ast, &res, iterf);
-		close(fd[!ir]);
-		exit(res);
-	}
-	return (pid);
-}*/
-
-/*static pid_t	new_fork(t_ast *ast, t_iterf *iterf, int fd[3])
-{
-	pid_t			pid;
-	int				ir;
-	short			a;
-	static pid_t	pgrp;
-
-	ir = (ast == ast->parent->right ? 1 : 0);
-	a = !(pgrp) ? 1 : 0;
-	if (ir && check_cond(ast, 0))
-		fd[2] = 1;
-	if ((g_shell->bits & (1 << 2)))
-		pgrp = getpgrp();
-	pid = son_fork(pgrp, fd, ast, iterf);
-	setpgid(pid, (!pgrp ? (pgrp = pid) : pgrp));
-	if (!(g_shell->bits & (1 << 2)) && a)
-		tcsetpgrp(0, pgrp);
-	g_shell->curpid = pgrp;
-	if (!ir && ast->type != TK_PIPE)
-		pgrp = 0;
-	return (pid);
-}*/
-
-/*static int		exec_left(int fd[3], pid_t pids[2], t_ast *ast, t_iterf *iterf)
-{
-	int	m;
-	int	res;
-
-	if (!fd[2] && check_cond(ast, 1))
-	{
-		if ((pids[0] = new_fork(ast->left, iterf, fd)) == -1)
-			if (!close(fd[0]) && !close(fd[1]))
-				return (SH_FORKFAIL);
-		if (!fd[2])
-			waitpid(pids[0], NULL, WUNTRACED);
-	}
-	else if (!fd[2])
-	{
-		m = dup(1);
-		close(fd[0]);
-		dup2(fd[1], 1);
-		g_shell->bits |= (1 << 1);
-		ft_astiter(ast->left, &res, iterf);
-		dup2(m, 1);
-		g_shell->bits &= ~(1 << 1);
-	}
-	return (0);
-}
-*/
 static void		print_tab_pipe(t_list *begin)
 {
 	t_list	*elem;
@@ -124,24 +34,6 @@ static void		print_tab_pipe(t_list *begin)
 		elem = elem->parent;
 	}
 }
-
-/*static pid_t	new_fork(t_ast *ast, t_iterf *iterf, int fd[3], int *a)
-{
-	pid_t			pid;
-	int				ir;
-	short			b;
-	static pid_t	pgrp;
-
-	b = !(pgrp) ? 1 : 0;
-	if (!pgrp)
-		pgrp = getpgrp();
-	son_fork(pgrp, fd, ast, iterf);
-	setpgid(pid, (!pgrp ? (pgrp = pid) : pgrp));
-	if (!(g_shell->bits & (1 << 2)) && b)
-		tcsetpgrp(0, pgrp);
-	g_shell->curpid = pgrp;
-	return (pid);
-}*/
 
 static void		swap1(int *fd)
 {
@@ -187,8 +79,6 @@ static int		son_action(int *fd, t_list *elem)
 	return (0);
 }
 
-#include <errno.h>
-
 void			closefd(int *fd, t_list *elem)
 {
 	if (!elem->next)
@@ -231,7 +121,9 @@ int				shell_pipe_cb(t_ast *ast, void **op, void *res, t_iterf *iterf)
 				return (SH_PIPFAIL);
 		if (!(pid = fork()))
 		{
-			setpgid(0, pgrp);
+			if (!(g_shell->bits & (1 << 2)))
+				setpgid(0, pgrp);
+			signal(SIGTSTP, SIG_DFL);
 			printfd(fd);
 			son_action(fd, elem);
 			ft_astiter((t_ast *)elem->content, res, iterf);
@@ -239,13 +131,17 @@ int				shell_pipe_cb(t_ast *ast, void **op, void *res, t_iterf *iterf)
 		}
 		else if (pid == -1)
 			return (SH_FORKFAIL);
-		setpgid(pid, (!pgrp ? pid : pgrp));
+		if (!(g_shell->bits & (1 << 2)))
+			setpgid(pid, (!pgrp ? pid : pgrp));
 		closefd(fd, elem);
-		if (!pgrp) /* add condition if no in bckground*/
+		if (!pgrp && !(g_shell->bits & (1 << 2)))
 		{
 			pgrp = pid;
+			handle_bgproc(pid, ((t_ast *)elem->content)->args->argv, BG_RUN, 1);
 			tcsetpgrp(0, pgrp);
 		}
+		else if (!pgrp && g_shell->bits & (1 << 2))
+			pgrp = -1;
 		elem->content_size = pid;
 		elem = elem->parent;
 		if (elem && elem->next && elem->next->next)
@@ -254,42 +150,15 @@ int				shell_pipe_cb(t_ast *ast, void **op, void *res, t_iterf *iterf)
 	elem = ft_lstend(tabpipe);
 	while (elem)
 	{
-		waitpid((pid_t)elem->content_size, res, WUNTRACED);
+		log_debug("WAITING...: %s pid: %d\n", ((t_ast *)elem->content)->name, elem->content_size);
+		if (waitpid((pid_t)elem->content_size, res, WUNTRACED) == -1)
+			log_debug("waitpid == -1\n");
+		log_debug("NAME_END: %s\n", ((t_ast *)elem->content)->name);
 		elem = elem->parent;
 	}
-	tcsetpgrp(0, getpgrp());
+	log_debug("ouiiiiiiiiii\n");
+	if (!(g_shell->bits & (1 << 2)))
+		tcsetpgrp(0, getpgrp());
 	g_shell->bits &= ~(1 << 1);
 	return (0);
 }
-
-/*int				shell_pipe_cb(t_ast *ast, void **op, void *res, t_iterf *iterf)
-{
-	int		fd[3];
-	pid_t	pids[2];
-	t_list	*tab_pipe;
-	int		ret;
-	static int	tutu;
-
-	(void)op;
-	tab_pipe = NULL;
-	ft_bzero(fd, sizeof(int) * 3);
-	if ((ret = handle_ast_pipe(ast, &tab_pipe)))
-		return (ret);
-	if (!tutu)
-		print_tab_pipe(tab_pipe);
-	tutu++;
-	if (pipe(fd) == -1)
-		return (SH_PIPFAIL);
-	if ((pids[1] = new_fork(ast->right, iterf, fd)) == -1)
-		if (!close(fd[0]) && !close(fd[1]))
-			return (SH_FORKFAIL);
-	if (exec_left(fd, pids, ast, iterf))
-		return (SH_FORKFAIL);
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pids[1], res, WUNTRACED);
-	*(int *)res = WEXITSTATUS(*(int *)res);
-	if (!(g_shell->bits & (1 << 2)))
-		tcsetpgrp(0, g_shell->shellpid);
-	return (0);
-}*/
