436aefc8e937e483b3ca3ee232a059bad12eec45
gdufay <gdufay@student.42.fr>
2018-10-16 12:33:51 +0200

    add backslash handling to libedit, home name when error cd

--- srcs/libedit/check.c
+++ srcs/libedit/check.c
@@ -6,21 +6,23 @@
 /*   By: gdufay <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 11:03:26 by gdufay            #+#    #+#             */
-/*   Updated: 2018/06/11 14:21:58 by gdufay           ###   ########.fr       */
+/*   Updated: 2018/10/16 12:22:18 by gdufay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libedit.h"
 
-int		ft_check_quote(char *s)
+int				ft_check_quote(char *s)
 {
 	int		i;
 	char	*tmp;
+	int		counter;
 
 	if (!s)
 		return (1);
 	i = -1;
 	tmp = s;
+	counter = 0;
 	while (tmp[++i])
 		if (tmp[i] == '"' || tmp[i] == 39 || tmp[i] == '`')
 		{
@@ -29,5 +31,9 @@ int		ft_check_quote(char *s)
 			tmp = ft_strchr(&tmp[i + 1], tmp[i]);
 			i = 0;
 		}
-	return (1);
+	if ((i = ft_strlen(s) - 1) < 0)
+		return (1);
+	while (tmp[i--] == '\\')
+		counter++;
+	return (counter % 2 ? 0 : 1);
 }
